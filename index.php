f2eed0b5c3b554db997ac91783ad14bbd29e0409
Filipe Pinho <filpsp2019@gmail.com>
2023-11-24 22:51:15 -0300

    Preparando para iniciar o projeto

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Teste</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php 
-        echo "Esta é uma página de teste em PHP";
-        echo "Branch de Filipe adicionada";
-        echo "Branch de Gabriel adicionada";
-    ?>
+    
 </body>
 </html>
